e6a0b104149fb9904d87147c1f579ae548071869
CYPARK <chanyoung990704@naver.com>
2025-04-11 22:13:01 +0900

    Post API 테스트 코드, CSRF 수정

--- src/main/java/org/example/demo/config/SecurityConfig.java
+++ src/main/java/org/example/demo/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                // .csrf(csrf -> csrf.disable()) // CSRF 비활성화
                 .authorizeHttpRequests(authz -> authz
                         // 게시글 조회 및 검색 허용
                         .requestMatchers(HttpMethod.GET, "/posts/{id:[0-9]+}", "/posts", "/posts/search").permitAll()
